2794268d67b5fef275cd86dbd580c192fba7c3e7
Franky777shg <frengky.sihombing.777@gmail.com>
2020-09-16 14:40:04 +0700

    start again

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,6 @@ const userController = require('./userController');
 const productController = require('./productController')
 
 //export all controllers
-// test
 module.exports = {
     userController,
     productController
