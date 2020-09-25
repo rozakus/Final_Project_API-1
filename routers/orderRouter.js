d26bad50b5f9f4f6ddaf2a741c053f68699f6a95
rozakus <abdulrozak.mail@gmail.com>
2020-09-25 16:47:55 +0700

    payment method

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -4,5 +4,6 @@ const { orderController } = require('../controllers')
 
 router.patch('/checkout/:on', orderController.checkOut)
 router.post('/payment', orderController.payment)
+router.get('/paymentMethod', orderController.paymentMethod)
 
 module.exports = router
